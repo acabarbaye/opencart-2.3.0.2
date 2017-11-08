8055e2d06b6168a982f4c1212a3c1b3d48cae4a4
beluybeluy <rulabeliy@gmail.com>
2017-11-08 19:20:36 +0200

    #upload files

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://a.lemarbet.com.ua/admin/');
+define('HTTP_CATALOG', 'http://a.lemarbet.com.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://a.lemarbet.com.ua/admin/');
+define('HTTPS_CATALOG', 'http://a.lemarbet.com.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/lemb8t/lemarbet.com.ua/a/admin/');
+define('DIR_SYSTEM', '/home/lemb8t/lemarbet.com.ua/a/system/');
+define('DIR_IMAGE', '/home/lemb8t/lemarbet.com.ua/a/image/');
+define('DIR_LANGUAGE', '/home/lemb8t/lemarbet.com.ua/a/admin/language/');
+define('DIR_TEMPLATE', '/home/lemb8t/lemarbet.com.ua/a/admin/view/template/');
+define('DIR_CONFIG', '/home/lemb8t/lemarbet.com.ua/a/system/config/');
+define('DIR_CACHE', '/home/lemb8t/lemarbet.com.ua/a/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/lemb8t/lemarbet.com.ua/a/system/storage/download/');
+define('DIR_LOGS', '/home/lemb8t/lemarbet.com.ua/a/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/lemb8t/lemarbet.com.ua/a/system/storage/modification/');
+define('DIR_UPLOAD', '/home/lemb8t/lemarbet.com.ua/a/system/storage/upload/');
+define('DIR_CATALOG', '/home/lemb8t/lemarbet.com.ua/a/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'lemb8t.mysql.ukraine.com.ua');
+define('DB_USERNAME', 'lemb8t_a');
+define('DB_PASSWORD', 'cs5lpxke');
+define('DB_DATABASE', 'lemb8t_a');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://a.lemarbet.com.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://a.lemarbet.com.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/lemb8t/lemarbet.com.ua/a/catalog/');
+define('DIR_SYSTEM', '/home/lemb8t/lemarbet.com.ua/a/system/');
+define('DIR_IMAGE', '/home/lemb8t/lemarbet.com.ua/a/image/');
+define('DIR_LANGUAGE', '/home/lemb8t/lemarbet.com.ua/a/catalog/language/');
+define('DIR_TEMPLATE', '/home/lemb8t/lemarbet.com.ua/a/catalog/view/theme/');
+define('DIR_CONFIG', '/home/lemb8t/lemarbet.com.ua/a/system/config/');
+define('DIR_CACHE', '/home/lemb8t/lemarbet.com.ua/a/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/lemb8t/lemarbet.com.ua/a/system/storage/download/');
+define('DIR_LOGS', '/home/lemb8t/lemarbet.com.ua/a/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/lemb8t/lemarbet.com.ua/a/system/storage/modification/');
+define('DIR_UPLOAD', '/home/lemb8t/lemarbet.com.ua/a/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'lemb8t.mysql.ukraine.com.ua');
+define('DB_USERNAME', 'lemb8t_a');
+define('DB_PASSWORD', 'cs5lpxke');
+define('DB_DATABASE', 'lemb8t_a');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
