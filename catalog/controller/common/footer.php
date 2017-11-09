1aa5bc47bcfa66f58b0d339fa08b6818d4d5f23e
beluybeluy <rulabeliy@gmail.com>
2017-11-09 10:07:31 +0200

    #add ajaxLiveSearch

--- catalog/controller/common/footer.php
+++ catalog/controller/common/footer.php
@@ -1,6 +1,16 @@
 <?php
 class ControllerCommonFooter extends Controller {
 	public function index() {
+		$this->load->language('product/search');
+                $data['text_empty'] = $this->language->get('text_empty');
+
+            	$data['text_view_all_results'] = $this->config->get('live_search_view_all_results')[$this->config->get('config_language_id')]['name'];
+                $data['live_search_ajax_status'] = $this->config->get('live_search_ajax_status');
+                $data['live_search_show_image'] = $this->config->get('live_search_show_image');
+                $data['live_search_show_price'] = $this->config->get('live_search_show_price');
+                $data['live_search_show_description'] = $this->config->get('live_search_show_description');
+                $data['live_search_href'] = $this->url->link('product/search', 'search=');
+                $data['live_search_min_length'] = $this->config->get('live_search_min_length');
 		$this->load->language('common/footer');
 
 		$data['scripts'] = $this->document->getScripts('footer');
